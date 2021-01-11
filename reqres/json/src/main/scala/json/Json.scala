d655d8ddee871655cb794dcf35507aa4eb9a7d12
AminMal <amin.malekloo@yahoo.com>
2021-01-11 21:18:07 +0330

    synched with optional-writer branch

--- reqres/json/src/main/scala/json/Json.scala
+++ reqres/json/src/main/scala/json/Json.scala
@@ -1,7 +1,6 @@
 package json
 
 object Json {
-  object CannotCastTypeException extends Exception("Cannot cast types")
   sealed trait JsonValue {
     def toString: String
   }
@@ -38,11 +37,11 @@ object Json {
         pair._2 != JsonNull
       }
         .map { pair =>
-        pair._2 match {
-          case JsonValueWrapperImpl(value) =>
-            "\"" + pair._1 + "\":" + value.toString
-        }
-      }.mkString("{", ",", "}")
+          pair._2 match {
+            case JsonValueWrapperImpl(value) =>
+              "\"" + pair._1 + "\":" + value.toString
+          }
+        }.mkString("{", ",", "}")
     }
   }
 
@@ -51,14 +50,11 @@ object Json {
   }
 
   trait Reader[T] {
-    def extractJson[V <: JsonValue](value: V): T
+    def extractJson(value: JsonValue): T
   }
 
   object Converter {
 
-    /**
-     * Basic writers
-     */
     implicit val stringWriter: Writer[String] = JsonString.apply
     implicit val intWriter: Writer[Int] = JsonInt
     implicit val floatWriter: Writer[Float] = JsonFloat
@@ -68,70 +64,8 @@ object Json {
     implicit def listWriter[T](implicit wjs: Writer[T]): Writer[List[T]] = (list: List[T]) => JsonArray(list.map(wjs.makeJson))
     implicit def arrayWriter[T](implicit wjs: Writer[T]): Writer[Array[T]] = (array: Array[T]) => JsonArray(array.map(wjs.makeJson))
 
-    /**
-     * Basic readers
-     */
-    implicit val stringReader: Reader[String] = new Reader[String] {
-      override def extractJson[V <: JsonValue](value: V): String = {
-        value match {
-          case s: JsonString => s.value
-          case _ => throw CannotCastTypeException
-        }
-      }
-    }
-    implicit val intReader: Reader[Int] = new Reader[Int] {
-      override def extractJson[V <: JsonValue](value: V): Int = {
-        value match {
-          case someInt: JsonInt =>
-            someInt.value
-          case _ => throw CannotCastTypeException
-        }
-      }
-    }
-    implicit val doubleReader: Reader[Double] = new Reader[Double] {
-      override def extractJson[V <: JsonValue](value: V): Double = {
-        value match {
-          case someDouble: JsonDouble =>
-            someDouble.value
-          case _ =>
-            throw CannotCastTypeException
-        }
-      }
-    }
-    implicit val floatReader: Reader[Float] = new Reader[Float] {
-      override def extractJson[V <: JsonValue](value: V): Float = {
-        value match {
-          case someFloat: JsonFloat =>
-            someFloat.value
-          case _ =>
-            throw CannotCastTypeException
-        }
-      }
-    }
-    implicit def seqReader[T](implicit rjs: Reader[T]): Reader[Seq[T]] = new Reader[Seq[T]] {
-      override def extractJson[V <: JsonValue](value: V): Seq[T] = {
-        value match {
-          case someArray: JsonArray =>
-            someArray.value.map(rjs.extractJson)
-          case _ => throw CannotCastTypeException
-        }
-      }
-    }
-    implicit def listReader[T](implicit rjs: Reader[T]): Reader[List[T]] = new Reader[List[T]] {
-      override def extractJson[V <: JsonValue](value: V): List[T] = {
-        value match {
-          case someArray: JsonArray =>
-            someArray.value.map(rjs.extractJson).toList
-          case _ => throw CannotCastTypeException
-        }
-      }
-    }
-
     implicit class ImplicitConverter[T](value: T) {
       def toJson(implicit wjs: Writer[T]): JsonValue = wjs.makeJson(value)
-    }
-    implicit class ImplicitReader[V <: JsonValue](value: V) {
-      def extractAs[T](implicit rjs: Reader[T]): T = rjs.extractJson(value)
     }
   }
 
